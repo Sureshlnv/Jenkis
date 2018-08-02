c445c1bb88107d0baed739c469e8a45337c86a03
JaswanthAbbineni <jaswanth.abbineni@gmail.com>
2018-08-02 15:00:13 +0530

    this code is commited by gayathri

---
+++ src/test/java/chiselon/Jenkins/Case1_test.java
@@ -0,0 +1,14 @@
+package chiselon.Jenkins;
+
+public class Case1_test {
+	
+	public void m1(){
+		
+		System.out.println("hi am m1 method");
+	}
+	public void m2(){
+		
+		System.out.println("hi am m2 method");
+	}
+
+}
---
+++ src/test/java/chiselon/Jenkins/Case2_test.java
@@ -0,0 +1,5 @@
+package chiselon.Jenkins;
+
+public class Case2_test {
+
+}
